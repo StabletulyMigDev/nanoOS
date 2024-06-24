584a3afa8249d4aaf1fbec7fd648b2dfab00be7a
StabletulyMigDev <mesquita_vicente@educadventista.org>
2024-06-24 13:01:50 -0300

    first commit

---
+++ kernelex/kernel.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stlib.h>
+#include "migrom.h"
+
+int main()
+{
+  prinf("kernelex started")
+  if(exists = migrom.h) {
+  printf("migrom runner 2.1 GNU/Mig")
+  }
+}
---
+++ kernelex/system.hpp
@@ -0,0 +1,3 @@
+#include "kernel.h"
+#include "migrom.h"
+#include <stlib.h>
